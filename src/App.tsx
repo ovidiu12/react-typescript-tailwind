78e02db65d734e7f9428268c89e7549696fd8d09
unknown <ovidiugr12@gmail.com>
2021-01-10 11:51:34 +0200

    small fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import * as React from "react";
-import "./styles.css";
+import "./styles/main.css";
 
-export default function App() {
+function App() {
   return (
-    <div className="App">
-      <h1>React Typescript Tailwind CSS started</h1>
-      <h2>Starter for TailwindCSS with React & Typescript</h2>
+    <div className="h-64">
+      <div className="p-4 m-4 bg-green-600">
+        <h1 className="text-2xl font-bold text-white">Tailwind CSS Demo</h1>
+      </div>
+      <div className="p-4 m-4 bg-green-500 h-full">
+        <h2 className="text-green-900">Have much fun using Tailwind CSS</h2>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          My Tailwind Button
+        </button>
+      </div>
     </div>
   );
 }
+
+export default App;
